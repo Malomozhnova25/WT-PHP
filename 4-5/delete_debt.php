7f4163e121b9d2a2daddf11c344b41d15ae2e537
Malomozhnova25 <80526435+Malomozhnova25@users.noreply.github.com>
2022-04-10 13:32:11 +0500

    4-5

---
+++ 4-5/delete_debt.php
@@ -0,0 +1,11 @@
+<? session_start(); 
+include("check_log.php");
+include("check_admin.php");
+ $conn=mysqli_connect("localhost", "a0657593_prop", "prop") or die ("Невозможно
+подключиться к серверу"); // установление соединения с сервером
+mysqli_select_db($conn,"a0657593_prop") or die ("Нет такой таблицы!");
+ $zapros="DELETE FROM debtors WHERE id_deb=" . $_GET['id'];
+ mysqli_query($conn,$zapros);
+ header("Location: index.php");
+ exit;
+?>
