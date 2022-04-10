7d33b797485d45630a7888e5fbb5dba41b2a7478
Malomozhnova25 <80526435+Malomozhnova25@users.noreply.github.com>
2022-04-10 13:32:49 +0500

    6 лр сессии

---
+++ 6/delete.php
@@ -0,0 +1,14 @@
+<? session_start(); 
+include("check_log.php");
+include("check_admin.php");
+
+
+
+ $conn=mysqli_connect("localhost", "a0657593_prop", "prop") or die ("Невозможно
+подключиться к серверу"); // установление соединения с сервером
+mysqli_select_db($conn,"a0657593_prop") or die ("Нет такой таблицы!");
+ $zapros="DELETE FROM property WHERE id=" . $_GET['id'];
+ mysqli_query($conn,$zapros);
+ header("Location: index.php");
+ exit;
+?>
